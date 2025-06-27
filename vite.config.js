ff9bb7646648501e8238e8ff6bfb9c446d690ebf
JasmineHu2025 <JasmineHu2025>
2025-06-27 16:23:09 +0800

    0627-configtest

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/crystalholic-teamproject/' : '/',
+  base: process.env.NODE_ENV === 'production'
+    ? '/crystalholic-teamproject/'
+    : '/',
   plugins: [react()],
 })
